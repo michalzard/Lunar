b429f6ce24c63728b9ec4595cb91c0dc2f62a84a
MichalPlatko <miso.platko@gmail.com>
2022-03-21 22:44:34 +0100

    header showing current display,arrow back,custom header elements

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../styles/components/Header.scss";
-import { Avatar,Typography } from '@mui/material';
+import { Avatar,Button,Typography } from '@mui/material';
+import ProfileDrawer from './mobile/ProfileDrawer';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
-function Header() {
+
+function Header({display,setDisplay}) {
+  const [opened,setOpen] = useState(false);
   return (
     <div className='header'>
-    <Avatar alt='User Avatar'/>
-    <Typography variant='h3' color='white'>Home</Typography>
+    {
+      display==='Home' ?
+      <Avatar alt='User Avatar' onClick={()=>{setOpen(true);}}/>
+      : <span onClick={()=>{setDisplay('Home');}}><ArrowBackIcon/></span>
+    }
+
+  {
+    display==='User Profile' ? 
+    <div className='info'>
+    <Typography variant='h6' color='white'>Username</Typography> 
+    <Typography variant='caption'>0 Posts</Typography> 
+    </div>
+    : 
+    <> 
+    {display==='Edit Profile' ? 
+    <div className='edit_profile'>
+    <Typography variant='h5' color='white'>{display}</Typography>
+    <Button variant='contained' className='edit_button'>Save</Button> 
+    </div> 
+    : <Typography variant='h5' color='white'>{display}</Typography>}
+    </>
+  }
+    <ProfileDrawer
+    anchor='left' 
+    open={opened}
+    setOpen={setOpen}
+    setDisplay={setDisplay}
+    />
+
     </div>
   )
 }
