82ff99e4cef78db59e5c616c401dabc7885cef35
MichalPlatko <miso.platko@gmail.com>
2022-03-24 18:35:12 +0100

    mobile date picker

--- src/components/ProfileEdit.jsx
+++ src/components/ProfileEdit.jsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
 import {Typography,TextField,Avatar} from '@mui/material';
+import {LocalizationProvider,MobileDatePicker} from '@mui/lab';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+
+
 import '../styles/components/ProfileEdit.scss';
 
 
@@ -9,16 +13,18 @@ function ProfileEdit() {
   const [bio,setBio] = useState('');
   const [location,setLocation] = useState('');
   const [web,setWeb] = useState('');
-
+  const [birthday,setBirthday] = useState(new Date());
   const updateUsername=e=>setUsername(e.target.value);
   const updateBio=e=>setBio(e.target.value);
   const updateLocation=e=>setLocation(e.target.value);
   const updateWeb=e=>setWeb(e.target.value);
-
+  const updateBirthday=e=>setBirthday(e);
 
   return (
     <div className='profile_edit'>
       <Avatar className='photo'/>
+      
+      <div className='fields'>
       <div className='name'>
       <Typography variant='body1'>Name</Typography>
       <TextField fullWidth variant='outlined' placeholder='Username' value={username} onChange={updateUsername}/>
@@ -36,7 +42,17 @@ function ProfileEdit() {
       <TextField fullWidth variant='outlined' placeholder='Web' value={web} onChange={updateWeb}/>
       </div>
       <div className='birthday'>
-      <Typography variant='body1'>Birth Date</Typography>
+
+      <Typography variant='body1' gutterBottom>Birth Date</Typography>
+
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <MobileDatePicker
+          label="Birthday"
+          value={birthday}
+          onChange={updateBirthday}
+          renderInput={(params)=><TextField {...params} focused/>}
+        /></LocalizationProvider>
+      </div>
       </div>
 
     </div>
