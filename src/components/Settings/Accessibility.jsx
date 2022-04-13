fe5993f64726f909a4c7e9dfae943622885a8e8f
MichalPlatko <miso.platko@gmail.com>
2022-04-13 11:47:39 +0200

    wip accessibility page

--- src/components/Settings/Accessibility.jsx
+++ src/components/Settings/Accessibility.jsx
@@ -1,25 +1,18 @@
 import React, { useState } from 'react';
 import '../../styles/components/Settings/Accessibility.scss';
-import {MobileStepper,Typography} from '@mui/material';
+import {Typography,Slider} from '@mui/material';
 
 function Accesibility() {
-  const [currentFontSize,setFontSize] = useState('12px');
-  const [currentStep,setCurrentStep] = useState(0);
+  
   return (
     <div className='accessibility'>
       <div className='example_post'>post</div>
       <div className='pick_fontsize'>
       <Typography className='smallerA'>Aa</Typography>
-      <MobileStepper 
-         variant="dots"
-        steps={6}
-        position="static"
-        activeStep={currentStep}
-        onClick={()=>{setCurrentStep(currentStep);}}
-        >
-      </MobileStepper>
+      <Slider step={1} marks min={12} max={20}  valueLabelDisplay='auto' />
       <Typography className='biggerA'>Aa</Typography>
       </div>
+
       <div className='pick_bgtheme'>
         bg theme
       </div>
