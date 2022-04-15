2ea9c44a77a150d4fd67f44379b1a69843972c56
MichalPlatko <miso.platko@gmail.com>
2022-04-15 20:56:32 +0200

    changes you can apply for specific user's display(wip)

--- src/components/Settings/Accessibility.jsx
+++ src/components/Settings/Accessibility.jsx
@@ -1,22 +1,30 @@
 import React, { useState } from 'react';
 import '../../styles/components/Settings/Accessibility.scss';
-import {Typography,Slider} from '@mui/material';
+import {Typography,Slider, Radio, Button} from '@mui/material';
 
 function Accesibility() {
-  
+  const [fontSize,setFontSize] = useState(10);
+  const [theme,setTheme] = useState('dark');//will load from localstorage
   return (
     <div className='accessibility'>
       <div className='example_post'>post</div>
       <div className='pick_fontsize'>
       <Typography className='smallerA'>Aa</Typography>
-      <Slider step={1} marks min={12} max={20}  valueLabelDisplay='auto' />
+      <Slider step={1} marks min={10} max={16} valueLabelDisplay='auto' onChange={(e)=>setFontSize(e.target.value)}/>
       <Typography className='biggerA'>Aa</Typography>
       </div>
-
+      <Typography variant='h6'>Color Theme</Typography>
       <div className='pick_bgtheme'>
-        bg theme
+        <div className='theme_block dark'>
+        <Typography variant='h6'>Dim</Typography>
+        <Radio  checked={theme==='dark'} value='dark' onChange={(e)=>{setTheme(e.target.value);}}/>
+        </div>
+        <div className='theme_block light'>
+        <Typography variant='h6'>Light</Typography>
+        <Radio checked={theme==='light'} value='light' onChange={(e)=>{setTheme(e.target.value);}}/>
+        </div>
       </div>
-    save button here
+    <Button variant='contained'>Done</Button>
     </div>
   )
 }
