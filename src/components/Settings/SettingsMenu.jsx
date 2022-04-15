b84dbfb364807490a3d40a3c44b83bd83bcaa517
MichalPlatko <miso.platko@gmail.com>
2022-04-15 20:56:56 +0200

    added descriptions to specific settings button method

--- src/components/Settings/SettingsMenu.jsx
+++ src/components/Settings/SettingsMenu.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import {Button, Typography} from '@mui/material';
+import React from 'react';
+import {Typography} from '@mui/material';
 import '../../styles/components/Settings/SettingsMenu.scss';
 import ArrowForward from '@mui/icons-material/ArrowForwardIos';
 
@@ -8,8 +8,9 @@ function SettingsMenu({setDisplay}) {
 
   return (
     <div className='settings_menu'>
-       <SettingsButton name='Account Information' setDisplay={setDisplay}/>
-       <SettingsButton name='Accessibility' setDisplay={setDisplay}/>
+       <SettingsButton name='Account Information' setDisplay={setDisplay} description='Edit account information like password and email address'/>
+       <SettingsButton name='Accessibility' setDisplay={setDisplay} description='Change font size and color theme for your background'/>
+       <SettingsButton name='Privacy and Safety' setDisplay={setDisplay} description='Manage muted or blocked words and accounts'/>
     </div>
   )
 }
@@ -17,11 +18,13 @@ function SettingsMenu({setDisplay}) {
 export default SettingsMenu;
 
 //highlights will be probably shown only on web view,on mobile,whole page is used to display content
-function SettingsButton({name,setDisplay}){
+function SettingsButton({name,setDisplay,description}){
   //checks if selected button matches name to apply highlighted css class
   return(
     <div className={`settingsButton`} onClick={()=>{setDisplay(`Settings-${name}`)}}>
-    {name ? <Typography >{name}</Typography> : 'undefined'} <ArrowForward />
+    <div className='info'>{name ? <Typography >{name}</Typography> : 'undefined'}
+    <Typography variant='caption'>{description ? description : null }</Typography>
+    </div> <ArrowForward />
     </div>
   )
 }
