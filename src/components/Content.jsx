ee7bec38b3e5648949fa6a704903b78953846fbe
MichalPlatko <miso.platko@gmail.com>
2022-04-21 02:44:37 +0200

    Routes now determine content displayed in main div container

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -10,38 +10,50 @@ import SettingsMenu from './Settings/SettingsMenu';
 import AccountInformation from './Settings/AccountInformation';
 import Acessibility from "./Settings/Accessibility";
 import Bookmarks from './Bookmarks';
+import {BrowserRouter as Router,Routes,Route, Navigate} from 'react-router-dom';
 
+function Content({display,setDisplay,isDisplay,setPostUnsaved,user}) {
+  // const [searchParams, setSearchParams] = useSearchParams();
 
-function Content({display,setDisplay,isDisplay,setPostUnsaved}) {
   return (
+    <Router>
     <div className='content'>
-    {isDisplay('home') ? 
-    <div className='_content'>
+    
+    <Routes>
+    <Route path='/' element={<Navigate to='/home'/>}></Route>
+    {/* <Route path='/home' element={}/> */}
+
+    <Route path='/u/:name' element={<UserProfile user={user} setDisplay={setDisplay}/>}/>
+    <Route path='/u/:name/edit' element={<ProfileEdit user={user} />}/>
+    <Route path='/post' element={<PostEditor setPostUnsaved={setPostUnsaved}/>}/>
+
+    <Route path='/home'  element={<><div className='_content'>
     <Typography variant='body1' color='white'>Content</Typography>
+    </div><Navbar display={display} setDisplay={setDisplay}/></>}/>
+
+    <Route path='/search' element={<Navbar display={display} setDisplay={setDisplay}/>}/>
+
+    <Route path='/settings' element={<SettingsMenu display={display} setDisplay={setDisplay} />}/>
+
+    <Route path='/bookmarks' element={<Bookmarks/>}/>
+    </Routes>
+
+    {/* {isDisplay('user profile') ? <UserProfile user={user} setDisplay={setDisplay}/> : null}   */}
+    {/* {isDisplay('edit profile') ? <ProfileEdit user={user} /> : null} */}
+    {/* {isDisplay('post editor') ? <PostEditor setPostUnsaved={setPostUnsaved}/> : null} */}
     
-    </div>
-    : null }
-    {isDisplay('user profile') ? <UserProfile setDisplay={setDisplay}/> : null}  
-    {isDisplay('edit profile') ? <ProfileEdit/> : null}
-    {isDisplay('post editor') ? <PostEditor setPostUnsaved={setPostUnsaved}/> : null}
-    
-    {
-    isDisplay('home') ?
-    <div className='fab'>
-    <Fab disableRipple onClick={()=>{setDisplay('Post Editor')}}> <HistoryEduIcon/> </Fab>
-    </div>
-    : null 
-    }
-    {isDisplay('home','search') ? <Navbar display={display} setDisplay={setDisplay}/> : null}
+  
+    {/* {isDisplay('home','search') ? <Navbar display={display} setDisplay={setDisplay}/> : null} */}
 
     
-    {isDisplay('settings') ? <SettingsMenu display={display} setDisplay={setDisplay} /> : null}
-    {isDisplay('settings-account information') ?  <AccountInformation/>: null}
+    {/* {isDisplay('settings') ? <SettingsMenu display={display} setDisplay={setDisplay} /> : null} */}
+    {isDisplay('settings-account information') ?  <AccountInformation user={user}/>: null}
     {isDisplay('settings-accessibility') ? <Acessibility/> : null}
 
-    {isDisplay('bookmarks') ? <Bookmarks/> : null}
+    {/* {isDisplay('bookmarks') ? <Bookmarks/> : null} */}
 
     </div>
+    </Router>
   )
 }
 
