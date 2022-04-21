626fd17ebb393da9b01ae5ec1c03704e72d26a79
MichalPlatko <miso.platko@gmail.com>
2022-04-21 02:44:57 +0200

    todo:how to handle pws

--- src/components/Settings/AccountInformation.jsx
+++ src/components/Settings/AccountInformation.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../../styles/components/Settings/AccountInformation.scss';
 import {Button, TextField, Typography} from '@mui/material';
 
-function AccountInformation() {
+function AccountInformation({user}) {
+  const [name,setName]=useState(user ? user.name : '');
+  const [email,setEmail]=useState(user ? user.email : '');
+  const [password,setPassword]=useState('');
+  const updateField=(e,setter)=>{setter(e.target.value);}
   return (
     <div className='account_information'>
     <Typography variant='caption' className='title'>Edit account information like password and email address</Typography>
     <div className='fields'>
-    <TextField placeholder='Name' type='text' fullWidth/>
-    <TextField placeholder='Email' type='email' fullWidth/>
-    <TextField placeholder='Password' type='password' fullWidth/>
+    <TextField placeholder='Name' type='text' fullWidth  onChange={(e)=>updateField(e,setName)}/>
+    <TextField placeholder='Email' type='email' fullWidth  onChange={(e)=>updateField(e,setEmail)}/>
+    <TextField placeholder='Password' type='password' fullWidth onChange={(e)=>updateField(e,setPassword)}/>
     </div>
     <Button variant='outlined' color='error'>Deactivate Account</Button>
     </div>
