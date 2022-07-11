7ea4b8274215300c51d868bc008ee4035379a4ce
Michalzard <miso.platko@gmail.com>
2022-07-11 02:43:50 +0200

    on desktop view,sidebars stay,content in middle determined by route

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/components/Content.scss";
 import { Typography, Fab, Button, Avatar, TextField } from "@mui/material";
 import HistoryEduIcon from "@mui/icons-material/HistoryEdu";
+import Header from "./Header";
 import UserProfile from "./Profile/UserProfile";
 import ProfileEdit from "./Profile/ProfileEdit";
 import PostEditor from "./Post/PostEditor";
@@ -9,7 +10,7 @@ import SettingsMenu from "./Settings/SettingsMenu";
 // import AccountInformation from './Settings/AccountInformation';
 // import Acessibility from "./Settings/Accessibility";
 import Bookmarks from "./Bookmarks";
-import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
+import { Routes, Route, Navigate, useNavigate, useLocation } from "react-router-dom";
 
 //Buttons
 import HomeIcon from "@mui/icons-material/Home";
@@ -19,79 +20,79 @@ import PersonIcon from "@mui/icons-material/Person";
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 //
 import SearchIcon from '@mui/icons-material/Search';
+import TrendingUpIcon from '@mui/icons-material/TrendingUp';
+import WhatshotIcon from '@mui/icons-material/Whatshot';
 
-function Content({isMobile,setPostUnsaved,user,filter,setFilter,lastSelectedMedia,setLastSelectedMedia,postText,setPostText,submitPost,postAlert}) {
+
+function Content({isMobile,isPostUnsaved,setPostUnsaved,user,setUser,filter,setFilter,lastSelectedMedia,setLastSelectedMedia,postText,setPostText,submitPost,postAlert}) {
   const navigate = useNavigate();
+  const {pathname} = useLocation();
+
   return (
     <div className="content">
-      <Routes>
-        <Route path="/home" element={<>
-              <div className="_content">
-                {isMobile ? null : 
-                  <div className="sidebar_left">
-                    <div className="links">
-                      <Button variant="text" color="info" size="large" startIcon={<HomeIcon />}>Home</Button>
-                      <Button variant="text" color="info" size="large" startIcon={<NotifsActive />}>Notifications</Button>
-                      <Button variant="text" color="info" size="large" startIcon={<BookmarkIcon />}>Bookmarks</Button>
-                      <Button variant="text" color="info" size="large" startIcon={<PersonIcon />}>Profile</Button>
-                      <ProfileButton user={user}/>
-                    </div>
-                  </div>
-                }
+       <div className="_content">
 
-                <main>
-                  {
-                    isMobile ? null : <PostEditor submitPost={submitPost} setPostUnsaved={setPostUnsaved} filter={filter} setFilter={setFilter} lastSelectedMedia={lastSelectedMedia} setLastSelectedMedia={setLastSelectedMedia} postText={postText} setPostText={setPostText} postAlert={postAlert} />
-                  }
-                  <div className="posts">
-                  <Typography variant="body1" color="white">/Home content</Typography>
+        {/* LEFT */}
+              {isMobile || pathname === "/login" ? null : 
+                <div className="sidebar_left">
+                  <div className="links">
+                    <Button variant="text" color="info" size="large" startIcon={<HomeIcon />} onClick={()=>{navigate("/home")}}>Home</Button>
+                    <Button variant="text" color="info" size="large" startIcon={<NotifsActive />} onClick={()=>{navigate("/notifications")}}>Notifications</Button>
+                    <Button variant="text" color="info" size="large" startIcon={<BookmarkIcon />} onClick={()=>{navigate("/bookmarks")}}>Bookmarks</Button>
+                    <Button variant="text" color="info" size="large" startIcon={<PersonIcon />} onClick={()=>{navigate(`/u/${user.name}`)}}>Profile</Button>
+                    <ProfileButton user={user}/>
                   </div>
-                </main>
-                {isMobile ?
-                <div className="fab" onClick={() => {navigate("/post");}}>
-                <Fab> <HistoryEduIcon /> </Fab>
                 </div>
-                : null}
+              }
 
-                {isMobile ? null : (
-                  <div className="sidebar_right">
-                    {/* Searchbar at top,Trends*/}
-                    <div className="searchbar"><TextField variant="outlined" placeholder="Search Lunar" InputProps={{startAdornment:(<SearchIcon/>)}} fullWidth  /> </div>
-                    <div className="trends"> 
-                    <Typography variant="h6">Trending</Typography>
-                    </div>
-                  </div>
-                )}
+              {/* MAIN CONTENT  */}
+              <main>
+              <Header isMobile={isMobile} setUser={setUser} user={user}  isPostUnsaved={isPostUnsaved} submitPost={submitPost}/>
+              <Routes>
+              <Route path="/home" element={<>
+              {isMobile ? null :
+              <PostEditor submitPost={submitPost} isMobile={isMobile} setPostUnsaved={setPostUnsaved} filter={filter} setFilter={setFilter} postAlert={postAlert} lastSelectedMedia={lastSelectedMedia} setLastSelectedMedia={setLastSelectedMedia} postText={postText} setPostText={setPostText}/>
+              }
+              <div className="posts">
+              <Typography>Posts will be displayed here.</Typography>
               </div>
-            </>
-          }
-        />
+              {isMobile ? <div className="fab" onClick={() => {navigate("/post");}}> <Fab> <HistoryEduIcon /> </Fab> </div> : null}
+              </>}/>
+              {/** REDIRECT BACK TO HOME IF NAME ISNT SPECIFIED /U/NAME */}
+              <Route path="/u/" element={<Navigate replace to="/home" />} />
 
-        {/** REDIRECT BACK TO HOME IF NAME ISNT SPECIFIED /U/NAME */}
-        <Route path="/u/" element={<Navigate replace to="/home" />} />
+              <Route path="/u/:name" element={<UserProfile isMobile={isMobile} user={user} />} />
+              <Route path="/u/:name/edit" element={<ProfileEdit isMobile={isMobile} user={user} />} />
+              {/* NO NEED FOR /POST,MAKE DIALOG POPUP WITH POST EDITOR (MOBILE ONLY) */}
+              <Route
+              path="/post"
+              element={
+              <PostEditor submitPost={submitPost} isMobile={isMobile} setPostUnsaved={setPostUnsaved} filter={filter} setFilter={setFilter} postAlert={postAlert} lastSelectedMedia={lastSelectedMedia} setLastSelectedMedia={setLastSelectedMedia} postText={postText} setPostText={setPostText}/>
+              }
+              />
+              <Route path="/settings" element={<SettingsMenu />} />
+              <Route path="/bookmarks" element={<Bookmarks />} />
+              </Routes>
+              </main>
 
-        <Route path="/u/:name" element={<UserProfile user={user} />} />
-        <Route path="/u/:name/edit" element={<ProfileEdit user={user} />} />
-        {/* NO NEED FOR /POST,MAKE DIALOG POPUP WITH POST EDITOR (MOBILE ONLY) */}
-        <Route
-          path="/post"
-          element={
-            <PostEditor
-              setPostUnsaved={setPostUnsaved}
-              filter={filter}
-              setFilter={setFilter}
-              postAlert={postAlert}
-              lastSelectedMedia={lastSelectedMedia}
-              setLastSelectedMedia={setLastSelectedMedia}
-              postText={postText}
-              setPostText={setPostText}
-            />
-          }
-        />
-        <Route path="/settings" element={<SettingsMenu />} />
-        <Route path="/bookmarks" element={<Bookmarks />} />
-      </Routes>
-    </div>
+              {/* RIGHT  */}
+              {isMobile || pathname === "/login" ? null : 
+                <div className="sidebar_right">
+                  {/* Searchbar at top,Trends*/}
+                  <div className="searchbar"><TextField variant="outlined" placeholder=" Search Lunar" InputProps={{startAdornment:(<SearchIcon/>)}} fullWidth  /> </div>
+                  <div className="trends"> 
+                  <div className="title"><TrendingUpIcon/><Typography variant="h6">Trending</Typography></div>
+                  <div className="previews">
+                   
+                  <Typography><WhatshotIcon/>No trends available</Typography>  
+                  </div>
+                  </div>
+                </div>
+              }
+              </div>   
+          
+              
+      </div>
   );
 }
 
