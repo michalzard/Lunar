1509f63caac2a982b11cb71b71715882467014c5
Michalzard <miso.platko@gmail.com>
2022-07-20 00:58:21 +0200

    menu function

--- src/components/Post/PostContainer.jsx
+++ src/components/Post/PostContainer.jsx
@@ -1,5 +1,5 @@
 import { Avatar, Backdrop, TextField, Button, Typography, Menu, MenuItem, Snackbar, Alert, Slide } from '@mui/material';
-import React,{useEffect, useState} from 'react'
+import React,{ useState} from 'react'
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import ModeCommentIcon from '@mui/icons-material/ModeComment';
 import RepeatIcon from '@mui/icons-material/Repeat';
@@ -9,18 +9,15 @@ import IosShareIcon from '@mui/icons-material/IosShare';
 import '../../styles/components/Post/PostContainer.scss';
 import CloseIcon from '@mui/icons-material/Close';
 import DefaultImage from "../../assets/lunarsystem.jpg";
+//menus
+import ActionMenu from './ActionMenu';
 //
 import LinkIcon from '@mui/icons-material/Link';
 import BookmarkAddIcon from '@mui/icons-material/BookmarkAdd';
 import BookmarkAddedIcon from '@mui/icons-material/BookmarkAdded';
 
-//TODO: 
-// make ... dropdown  - follow,bookmark,mute,block,report
-// share button  - dropdown menu with (copy link to post,bookmark)
-
 function PostContainer({isMobile,post}) {
 
-  const openActionMenu=(e)=>{e.preventDefault();console.log("Action Menu Dropdown");}
   const [previewOpen,setPreviewOpen] = useState(false);
 
   //buttons
@@ -32,6 +29,7 @@ function PostContainer({isMobile,post}) {
 
   
   const [shareMenuAnchor,setShareMenuAnchor] = useState(null);
+  const [actionMenuAnchor,setActionMenuAnchor] = useState(null);
 
   return (
     <div className='post_container'>
@@ -48,12 +46,14 @@ function PostContainer({isMobile,post}) {
     </div>
 
     <div className='author'>
-    {post ? <>
+    {post ?
+    <>
     <Typography className='name'>{post.author.name}</Typography>
     <div className='tagAndTime'>
     <Typography variant="caption" className='tag'>@{post.author.name}</Typography>
     <Typography variant="caption" className="timeAgo"> ~ 7m</Typography></div>
-    </> : null} 
+    </> 
+    : null} 
     
     
     </div>
@@ -69,18 +69,20 @@ function PostContainer({isMobile,post}) {
     <RepeatIcon style={{color:reposted ? "greenyellow" : null}} />
     <Typography className='likeCount' component="span">{repostCount > 0 ? repostCount : null}</Typography></div>
 
-    <div className='share'><IosShareIcon onClick={(e)=>{setShareMenuAnchor(e.currentTarget);}}/></div>
+    <div className='share' onClick={(e)=>{setShareMenuAnchor(e.currentTarget);}}><IosShareIcon/></div>
     <ShareMenu anchor={shareMenuAnchor} setShareMenuAnchor={setShareMenuAnchor}/>
+    <ActionMenu isMobile={isMobile} anchor={actionMenuAnchor} setActionMenuAnchor={setActionMenuAnchor} author={post.author}/>
     </div>
 
     </div>
 
     <div className='right'>
-    <MoreHorizIcon className="more" onClick={openActionMenu}/>
+    <MoreHorizIcon className="more" onClick={(e)=>{setActionMenuAnchor(e.currentTarget);}}/>
     </div>
     <PostPreview isMobile={isMobile} post={post}  open={previewOpen} reposted={reposted} liked={liked} 
-    setOpen={setPreviewOpen} setLiked={setLiked} setReposted={setReposted} 
+    setOpen={setPreviewOpen} setLiked={setLiked} setReposted={setReposted} setShareMenuAnchor={setShareMenuAnchor} setActionMenuAnchor={setActionMenuAnchor}
     />
+    
     </div>
   )
 }
@@ -91,8 +93,8 @@ export default PostContainer;
  * (Desktop Only) For Image,Have preview with backdrop that show image,comment section on the right
  * (Mobile Only) For All Media, have Post on top and comment section right below it
  */
-function PostPreview({isMobile,post,open,setOpen,reposted,setReposted,liked,setLiked}){
-  const fakeComments=[
+function PostPreview({isMobile,post,open,setOpen,reposted,setReposted,liked,setLiked,setShareMenuAnchor,setActionMenuAnchor}){
+  const fakeCommentArray=[
     "Wow","Wow","Wow","Wow","Wow","Wow","Wow","Wow","Wow","Wow","Wow","Wow","Wow","Wow","Wow","Wow","Wow",
   ]
   
@@ -147,7 +149,7 @@ function PostPreview({isMobile,post,open,setOpen,reposted,setReposted,liked,setL
     <div className='likes' onClick={()=>{setLiked(!liked);}}>{liked ? <FavoriteIcon style={{color:"red"}} /> : <FavoriteBorderIcon />}
     <span className='likeCount'></span></div>
     <div className='reposts' onClick={()=>{setReposted(!reposted);}}> <RepeatIcon style={{color:reposted ? "greenyellow" : null}} /><span className='repostCount'></span></div>
-    <div className='share'><IosShareIcon/></div>
+    <div className='share' onClick={(e)=>{setShareMenuAnchor(e.currentTarget);}}><IosShareIcon/></div>
     </div>
     </div>
     </div>
@@ -159,8 +161,8 @@ function PostPreview({isMobile,post,open,setOpen,reposted,setReposted,liked,setL
     <section className='comment_section' id="comment_section">
 
       {
-        fakeComments.map((comment,i)=>{
-          return <PostComment key={i} comment={post} liked={liked} setLiked={setLiked} reposted={reposted} setReposted={setReposted} />
+        fakeCommentArray.map((comment,i)=>{
+          return <PostComment key={i} comment={post} liked={liked} setLiked={setLiked} reposted={reposted} setReposted={setReposted} setShareMenuAnchor={setShareMenuAnchor} setActionMenuAnchor={setActionMenuAnchor}/>
         })
       }
 
@@ -173,7 +175,7 @@ function PostPreview({isMobile,post,open,setOpen,reposted,setReposted,liked,setL
 }
 
 
-function PostComment({comment,liked,setLiked,reposted,setReposted}){
+function PostComment({comment,liked,setLiked,reposted,setReposted,setShareMenuAnchor,setActionMenuAnchor}){
   
   return(
     <div className='post_container'>
@@ -202,46 +204,45 @@ function PostComment({comment,liked,setLiked,reposted,setReposted}){
   <div className='likes' onClick={()=>{setLiked(!liked);}}>{liked ? <FavoriteIcon style={{color:"red"}} /> : <FavoriteBorderIcon />}
   <span className='likeCount'></span></div>
   <div className='reposts' onClick={()=>{setReposted(!reposted);}}> <RepeatIcon style={{color:reposted ? "greenyellow" : null}} /><span className='repostCount'></span></div>
-  <div className='share'><IosShareIcon/></div>
+  <div className='share' onClick={(e)=>{setShareMenuAnchor(e.currentTarget);}}><IosShareIcon/></div>
   </div>
 
   </div>
   <div className='right'>
-  <MoreHorizIcon className="more"/>
+  <MoreHorizIcon className="more" onClick={(e)=>{setActionMenuAnchor(e.currentTarget);}}/>
   </div>
   </div>
   )
 }
 
 
+
+
 function ShareMenu({anchor,setShareMenuAnchor}){
-  const onClose=()=>{
-    setShareMenuAnchor(null);
-  }
   const [bookmarked,setBookmarked] = useState(false); 
-
-  const copyURLtoClipboard=()=>{
-    window.navigator.clipboard.writeText(window.location.href);
-  }
-
   const [alertOpen,setAlertOpen] = useState(false);
   const closeAlert=()=>{
     setAlertOpen(false);
   }
+  const menuClose=()=>{
+    setShareMenuAnchor(null);
+  }
+  const copyURLtoClipboard=()=>{
+    window.navigator.clipboard.writeText(window.location.href);
+  }
+
   return (
     <Menu
     open={Boolean(anchor)}
     anchorEl={anchor}
-    onClose={onClose}
+    onClose={menuClose}
     id="ShareMenu"
     >
     <MenuItem onClick={()=>{copyURLtoClipboard();setAlertOpen(true);}}><LinkIcon/> Copy Link</MenuItem>
     <MenuItem onClick={()=>{bookmarked ? setBookmarked(false) : setBookmarked(true);}}>
-    {
-      bookmarked ? <BookmarkAddedIcon style={{color:"greenyellow"}}/> 
-      :  <BookmarkAddIcon/>   
-    }
-    Add to Bookmark</MenuItem>
+    { bookmarked ? <BookmarkAddedIcon style={{color:"greenyellow"}}/> : <BookmarkAddIcon/> }
+    { bookmarked ? "Added to Bookmark":"Add to Bookmark"}
+    </MenuItem>
     <Snackbar
     anchorOrigin={{vertical:"bottom",horizontal:"right"}}
     open={alertOpen}
@@ -249,8 +250,10 @@ function ShareMenu({anchor,setShareMenuAnchor}){
     onClose={closeAlert}
     TransitionComponent={Slide}
     >
-      <Alert color="success">Post's URL Copied</Alert>
+    <Alert color="success">Post's URL has been copied</Alert>
     </Snackbar>
     </Menu>
   )
 }
+
+
